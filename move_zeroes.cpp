60cf749e1e18fddc46f5063d3ae4d27da9696d32
shahid-dev08 <shahidh.work@gmail.com>
2026-05-13 16:49:13 +0530

    Add move zeroes solution

---
+++ move_zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+        for(int i = 0;i < n;i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        for(;count<n;count++)
+        {
+            nums[count] = 0;
+        }
+    }
+};
