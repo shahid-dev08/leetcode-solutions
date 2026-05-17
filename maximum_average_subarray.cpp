f1eff786a6a0e6b7fb328a8f0e21a089aba12063
shahid-dev08 <shahidh.work@gmail.com>
2026-05-17 17:09:59 +0530

    Add max average subarray solution

---
+++ maximum_average_subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i = 0 ; i<k;i++)
+        {
+            sum += nums[i];
+        }
+        double max = sum;
+
+        for(int i = k;i<n;i++)
+        {
+            sum += nums[i];
+            sum -= nums[i-k];
+            if(sum > max) max = sum;
+        }
+
+        return max/k;
+    }
+};
